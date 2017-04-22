32447746c87c3ce39efcd7aa9d89501d8dfd4cf3
irfanalidv <irfanali29@hotmail.com>
2017-04-22 20:38:34 +0530

    Mahalanobis distance between string #5

---
+++ src/Mahalanobis distance_1.R
@@ -0,0 +1,31 @@
+lenLongStr <- function(String1, String2) {
+
+  
+  s1 <- unlist(strsplit(String1,split=""))
+  s2 <- unlist(strsplit(String2,split=""))
+  
+  num <- matrix(0, nchar(String1), nchar(String2))
+  maxlen <- 0
+  
+  for (i in 1:nchar(String1)) {
+    for (j in 1:nchar(String2)) {
+      if (s1[i] == s2[j]) {
+        if ((i==1) || (j==1)) {
+          num[i,j] <- 1
+        } 
+        else {
+          num[i,j] <- 1+num[i-1,j-1]
+        }
+        if (num[i,j] > maxlen) {
+          maxlen <- num[i,j]
+        }
+      }
+    }
+  }
+  maxlen
+}
+
+editDistance <- function (a, b) {
+  print(c(a, b, nchar(a) + nchar(b) - 2 * lenLongStr(a, b)))
+  nchar(a) + nchar(b) - 2 * lenLongStr(a, b)
+}
